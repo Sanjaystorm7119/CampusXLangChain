08a24e492602f5c8752ef66f36f1f1a1f1ae74f3
Sanjay b <sanjaysrivastavab@gmail.com>
2025-11-12 12:14:54 -0800

    revise LL

--- LangChainModels/1_LLMs/llm_demo.py
+++ LangChainModels/1_LLMs/llm_demo.py
@@ -1,10 +1,15 @@
-from langchain_openai import OpenAI
+# from langchain_openai import OpenAI
+from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-llm = OpenAI(model="gpt-3.5-turbo-instruct")
-inputstr = input("enter prompt: ")
-result = llm.invoke(inputstr)
+# llm = OpenAI(model="gpt-3.5-turbo-instruct")
+llm = GoogleGenerativeAI(model="gemini-2.5-flash")
 
-print(f"result : {result}")
+ 
+inputstr = input("enter prompt: ")
+while inputstr not in ["exit","bye"]:
+    result = llm.invoke(inputstr)
+    print(f"result : {result}")
+    inputstr = input("enter prompt: ")
