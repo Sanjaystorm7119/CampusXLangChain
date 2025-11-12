06e524a31fbe8a565e1f3e27621bf6e5dda9c3ce
Sanjay b <sanjaysrivastavab@gmail.com>
2025-11-12 12:16:43 -0800

    revert

--- LangChainModels/1_LLMs/llm_demo.py
+++ LangChainModels/1_LLMs/llm_demo.py
@@ -1,11 +1,11 @@
-# from langchain_openai import OpenAI
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_openai import OpenAI
+# from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
-# llm = OpenAI(model="gpt-3.5-turbo-instruct")
-llm = GoogleGenerativeAI(model="gemini-2.5-flash")
+llm = OpenAI(model="gpt-3.5-turbo-instruct")
+# llm = GoogleGenerativeAI(model="gemini-2.5-flash")
 
  
 inputstr = input("enter prompt: ")
