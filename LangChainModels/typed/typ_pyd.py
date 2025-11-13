251addca295ca022af8c70785a230e996d4b1ed2
Sanjay b <sanjaysrivastavab@gmail.com>
2025-11-13 12:09:05 -0800

    typedDict , annotated , pydantic in LC

--- LangChainModels/typed/typ_pyd.py
+++ LangChainModels/typed/typ_pyd.py
@@ -8,19 +8,19 @@ model = ChatOpenAI()
 class Review(BaseModel):
     key_theme: str = Field(description="A brief summary of the review")   
     summary: str = Field(description="A brief summary of the review")
-    sentiment: Literal["pos", "neg", "neutral"] = Field(description="The sentiment of the review, e.g., positive, negative, neutral")
+    sentiment: Literal["pos", "neg", "neutral"] = Field(description="The sentiment of the review, e.g., pos, neg, neutral")
     pros: Optional[list[str]] = Field(default=None, description="Pros of the product") 
     cons: Optional[list[str]] = Field(default=None, description="Cons of the product")
     name :Optional[str] = Field(default=None, description="Name of the reviewer")
 
-structured_model =model.with_structured_output(Review)
+structured_model =model.with_structured_output(Review,method="function_calling")
 
 result = structured_model.invoke("""the hardware is great, but the software is terrible.
                           there is no way to use it without a phone app, which is very buggy.
                         I would not recommend this product to anyone.
-                                 My name is John Doe.
+                                 review by is John Doe.
                                  """)
-
-print(result.name)
-# print(result['summary'])
+result = result.model_dump()
+print(result)
+print(result['summary'])
 # print(result['sentiment'])  
--- LangChainModels/typed/typeddic.py
+++ LangChainModels/typed/typeddic.py
@@ -1,15 +1,15 @@
 from langchain_openai import ChatOpenAI
 from langchain_google_genai import ChatGoogleGenerativeAI
 from google.generativeai import generative_models 
-from typing import TypedDict
-from pydantic import BaseModel
+from typing import TypedDict , Annotated , Literal
+from pydantic import BaseModel , Field 
 from dotenv import load_dotenv
 load_dotenv()
 
 # model = ChatOpenAI()
 # class Review(TypedDict):
-#     summary: str
-#     sentiment: str
+#     summary: Annotated[str,"a brief summary of review"]
+#     sentiment: Annotated[str,"overall sentiment of review , either pos/neg/neutral"]
 
 
 # structured_model =model.with_structured_output(Review)
@@ -18,10 +18,11 @@ load_dotenv()
 #                           there is no way to use it without a phone app, which is very buggy.
 #                         I would not recommend this product to anyone.""")
 
+
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 class Review(BaseModel):
-    summary: str
-    sentiment: str
+    summary: str = Field(description="overall description")
+    sentiment: Literal["pos", "neg", "neutral"] = Field(description="The sentiment of the review, e.g., positive, negative, neutral")
 
 
 structured_model =model.with_structured_output(Review)
@@ -29,5 +30,6 @@ structured_model =model.with_structured_output(Review)
 result = structured_model.invoke("""the hardware is great, but the software is terrible.
                           there is no way to use it without a phone app, which is very buggy.
                         I would not recommend this product to anyone.""")
-
-print(result)
+result=result.model_dump()
+# print(result.model_dump())
+print(f"sentiment :{result['sentiment']}\nsummary : {result['summary']}")
